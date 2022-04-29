6c61bb78c67e2cf72286ea9dbb2e2fe3059a0e6c
Oleg-ISS <62930142+Oleg-ISS@users.noreply.github.com>
2022-04-29 11:30:06 +0300

    Update creating_template.py

--- creating_template.py
+++ creating_template.py
@@ -1,1 +1,35 @@
-
+import openpyxl 
+import re
+#
+#
+#
+#
+#берем из ячейки "С17" содержимое, это строковая переменная
+book = openpyxl.open("invoice.xlsx", read_only=True)
+sheet = book.active
+a = sheet["C17"].value
+print('Содержимое ячейки С17 ',a)
+print(type(a))
+#меняем в этой переменной символ "x" на символ "\D"
+#!!!!! символ х из кирилической раскладки
+x = 'х'
+print('Меняем символ из кирилицы "х" на "\D" ',re.sub(x,r'\\D',a))
+print('Выполним проверку на соответствие с помощию match = re.search(a, b)')
+match = re.search(re.sub(x,r'\\D',a), a)
+print(match[0] if match else 'Not found')
+#
+#
+#
+#
+#берем из ячейки "С1" содержимое, это строковая переменная
+book = openpyxl.open("invoice.xlsx", read_only=True)
+sheet = book.active
+a = sheet["C1"].value
+print('Содержимое ячейки С1 ',a)
+print(type(a))
+#меняем в этой переменной символ "*" на символ "\D"
+x = '\*'
+print('Меняем символ из кирилицы "*" на "\D" ',re.sub(x,r'\\D',a))
+print('Выполним проверку на соответствие с помощию match = re.search(a, b)')
+match = re.search(re.sub(x,r'\\D',a), a)
+print(match[0] if match else 'Not found')

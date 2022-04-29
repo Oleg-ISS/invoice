caec818f8898b0ba7824d989cd56e3bfd33ca19f
Oleg-ISS <62930142+Oleg-ISS@users.noreply.github.com>
2022-04-29 16:58:47 +0300

    Update creating_template.py

--- creating_template.py
+++ creating_template.py
@@ -1,35 +1,47 @@
 import openpyxl 
 import re
-#
-#
-#
-#
-#берем из ячейки "С17" содержимое, это строковая переменная
-book = openpyxl.open("invoice.xlsx", read_only=True)
-sheet = book.active
-a = sheet["C17"].value
-print('Содержимое ячейки С17 ',a)
-print(type(a))
-#меняем в этой переменной символ "x" на символ "\D"
-#!!!!! символ х из кирилической раскладки
-x = 'х'
-print('Меняем символ из кирилицы "х" на "\D" ',re.sub(x,r'\\D',a))
-print('Выполним проверку на соответствие с помощию match = re.search(a, b)')
-match = re.search(re.sub(x,r'\\D',a), a)
-print(match[0] if match else 'Not found')
-#
-#
-#
-#
-#берем из ячейки "С1" содержимое, это строковая переменная
-book = openpyxl.open("invoice.xlsx", read_only=True)
-sheet = book.active
-a = sheet["C1"].value
-print('Содержимое ячейки С1 ',a)
-print(type(a))
-#меняем в этой переменной символ "*" на символ "\D"
-x = '\*'
-print('Меняем символ из кирилицы "*" на "\D" ',re.sub(x,r'\\D',a))
-print('Выполним проверку на соответствие с помощию match = re.search(a, b)')
-match = re.search(re.sub(x,r'\\D',a), a)
-print(match[0] if match else 'Not found')
+def pattern():
+    #извлекаем содержимое ячейки sheet.cell(row=r, column=c).value
+    r=1
+    c=2
+    book = openpyxl.open("invoice.xlsx", read_only=True)
+    sheet = book.active
+    print(sheet.cell(row=r, column=c).value)
+    print(re.sub('O-ring','Кольцо круглого сечения',sheet.cell(row=r, column=c).value))
+    #
+    #
+    #
+    #
+    #берем из ячейки sheet.cell(row=r, column=c+1).value содержимое, это строковая переменная
+
+    a = sheet.cell(row=r, column=c+1).value
+    print('Содержимое ячейки ',a)
+    print(type(a))
+    #меняем в этой переменной символ "x" на символ "\D"
+    #!!!!! символ х из кирилической раскладки
+    x = 'х'
+    print('Меняем символ из кирилицы "х" на "\D" ',re.sub(x,r'\\D',a))
+    print('Выполним проверку на соответствие с помощию match = re.search(a, b)')
+    match = re.search(re.sub(x,r'\\D',a), a)
+    print(match[0] if match else 'Not found')
+    #
+    #
+    #
+    #
+    #берем из ячейки sheet.cell(row=r, column=c+1).value содержимое, это строковая переменная
+    a = sheet.cell(row=r, column=c+1).value
+    print('Содержимое ячейки ',a)
+    print(type(a))
+    #меняем в этой переменной символ "*" на символ "\D"
+    x = '\*'
+    print('Меняем символ из кирилицы "*" на "\D" ',re.sub(x,r'\\D',a))
+    print('Выполним проверку на соответствие с помощию match = re.search(a, b)')
+    match = re.search(re.sub(x,r'\\D',a), a)
+    print(match[0] if match else 'Not found')
+    #
+    #
+    #
+    #объединяем название товара и шаблон размера в одну строку
+    return re.sub('O-ring','Кольцо круглого сечения',sheet.cell(row=r, column=c).value)+' '+a
+print(pattern())
+

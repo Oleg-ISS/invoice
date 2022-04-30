0d8d13b9dffcf7c9b4eafb42a64ba72f55ba8af4
Oleg-ISS <62930142+Oleg-ISS@users.noreply.github.com>
2022-04-30 19:25:05 +0200

    Update cycle.py

--- cycle.py
+++ cycle.py
@@ -5,6 +5,7 @@
 def(str)
     #открываем таблицу DB.xlsx для поиска в ней нужной строки
     book = openpyxl.load_workbook("DB.xlsx", read_only=true)
+    sheet = book.active
     #получаем количество строк на рабочем листе
     i = book.max_row
     #запускаем цикл поиска подходящей строки

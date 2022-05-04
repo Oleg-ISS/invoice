b17ed5eb2421ecd8f83cd27080cff385ae389987
Oleg-ISS <62930142+Oleg-ISS@users.noreply.github.com>
2022-05-04 20:25:05 +0200

    Update searching.py

--- searching.py
+++ searching.py
@@ -2,15 +2,29 @@
 #функция принимает строку шаблон
 #функция
 #функция ищет подходящую под шаблон ячейку и возвращает ее номер
-def searching(str)
+def searching(template)
     r = 1
+    с = 3
     #открываем таблицу DB.xlsx для поиска в ней нужной строки
     book = openpyxl.load_workbook("DB.xlsx", read_only=true)
     sheet = book.active
+    
     #получаем количество строк на рабочем листе
     i = sheet.max_row
     #запускаем цикл поиска подходящей строки
     for row in range(1,i+1):
-    #если строка найдена , то запускаем функцию создания строки в файле moysklad.xlsx
+        #обнуляем переменную match в каждой итерации
+        match = 0
+        #извлекаем из ячейки строку для сравнения
+        sheet.cell(row=r, column=c).value
+        #сравниваем строку в таблице "DB.xlsx" с шаблоном template
+        match = re.search(template,sheet.cell(row=r, column=c).value)
+        #если строка найдена match не равен 0, то запускаем функцию creating_string_in_template_file() создания строки в файле moysklad.xlsx
+        if match == 0:
+            #
+            creating_dont_search_string(r)
+        if match != 0:
+            creating_string_in_template_file()
     # Close the workbook after reading
-    openpyxl.close()
+    
+    book.close()

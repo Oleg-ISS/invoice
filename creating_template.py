3ae984a7c4d0a9f22c7d8d3de22926dadc711fa5
Oleg-ISS <62930142+Oleg-ISS@users.noreply.github.com>
2022-05-04 08:13:32 +0200

    Update creating_template.py

--- creating_template.py
+++ creating_template.py
@@ -3,7 +3,7 @@ import re
 def pattern():
     #извлекаем содержимое ячейки sheet.cell(row=r, column=c).value
     r=1
-    c=2
+    c=1
     book = openpyxl.open("invoice.xlsx", read_only=True)
     sheet = book.active
     print(sheet.cell(row=r, column=c).value)
@@ -28,20 +28,16 @@ def pattern():
     #
     #
     #
-    #берем из ячейки sheet.cell(row=r, column=c+1).value содержимое, это строковая переменная
-    a = sheet.cell(row=r, column=c+1).value
-    print('Содержимое ячейки ',a)
-    print(type(a))
     #меняем в этой переменной символ "*" на символ "\D"
     x = '\*'
     print('Меняем символ из кирилицы "*" на "\D" ',re.sub(x,r'\\D',a))
-    print('Выполним проверку на соответствие с помощию match = re.search(a, b)')
-    match = re.search(re.sub(x,r'\\D',a), a)
-    print(match[0] if match else 'Not found')
+    a = re.sub(x,r'\\D',a)
+    #print('Выполним проверку на соответствие с помощию match = re.search(a, b)')
+    #match = re.search(re.sub(x,r'\\D',a), a)
+    #print(match[0] if match else 'Not found')
     #
     #
     #
     #объединяем название товара и шаблон размера в одну строку
     return re.sub('O-ring','Кольцо круглого сечения',sheet.cell(row=r, column=c).value)+' '+a
 print(pattern())
-

bd34c03d059814bc9be3162053362ef9607b2d85
Oleg-ISS <62930142+Oleg-ISS@users.noreply.github.com>
2022-05-04 15:24:24 +0300

    Update cycle.py

--- cycle.py
+++ cycle.py
@@ -1,8 +1,9 @@
-#начало функции cycle
+#начало функции searching
 #функция принимает строку шаблон
-#функция ничего не возвращает
+#функция
 #функция ищет подходящую под шаблон ячейку и возвращает ее номер
-def(str)
+def searching(str)
+    r = 1
     #открываем таблицу DB.xlsx для поиска в ней нужной строки
     book = openpyxl.load_workbook("DB.xlsx", read_only=true)
     sheet = book.active

290988f9ce958638ee1c6ed69e84bc07f586f7d6
Oleg-ISS <62930142+Oleg-ISS@users.noreply.github.com>
2022-05-04 08:36:14 +0200

    Update creating_template.py

--- creating_template.py
+++ creating_template.py
@@ -1,28 +1,32 @@
 import openpyxl 
 import re
 def pattern():
+    #берем название изделия
     #извлекаем содержимое ячейки sheet.cell(row=r, column=c).value
     r=1
     c=1
     book = openpyxl.load_workbook("invoice.xlsx", read_only=True)
     sheet = book.active
     print(sheet.cell(row=r, column=c).value)
-    print(re.sub('O-ring','Кольцо круглого сечения',sheet.cell(row=r, column=c).value))
+    #По очереди пытаемся заменить название изделия с поставщика на наше, если получиться то меняем если нет то остается как было
+    item = re.sub('O-ring','Кольцо круглого сечения',sheet.cell(row=r, column=c).value)
+    item = re.sub('Oil Seal','Манжета армированная',sheet.cell(row=r, column=c).value)
+    print(item)
     #
     #
     #
-    #
+    #берем размер
     #берем из ячейки sheet.cell(row=r, column=c+1).value содержимое, это строковая переменная
 
-    a = sheet.cell(row=r, column=c+1).value
-    print('Содержимое ячейки ',a)
-    print(type(a))
+    size = sheet.cell(row=r, column=c+1).value
+    print('Содержимое ячейки ',size)
+    print(type(size))
     #меняем в этой переменной символ "x" на символ "\D"
     #!!!!! символ х из кирилической раскладки
     x = 'х'
-    print('Меняем символ из кирилицы "х" на "\D" ',re.sub(x,r'\\D',a))
-    print('Выполним проверку на соответствие с помощию match = re.search(a, b)')
-    match = re.search(re.sub(x,r'\\D',a), a)
+    print('Меняем символ из кирилицы "х" на "\D" ',re.sub(x,r'\\D',size))
+    print('Выполним проверку на соответствие с помощию match = re.search(size, b)')
+    match = re.search(re.sub(x,r'\\D',size), size)
     print(match[0] if match else 'Not found')
     #
     #
@@ -30,14 +34,18 @@ def pattern():
     #
     #меняем в этой переменной символ "*" на символ "\D"
     x = '\*'
-    print('Меняем символ из кирилицы "*" на "\D" ',re.sub(x,r'\\D',a))
-    a = re.sub(x,r'\\D',a)
-    #print('Выполним проверку на соответствие с помощию match = re.search(a, b)')
-    #match = re.search(re.sub(x,r'\\D',a), a)
+    print('Меняем символ из кирилицы "*" на "\D" ',re.sub(x,r'\\D',size))
+    size = re.sub(x,r'\\D',size)
+    #print('Выполним проверку на соответствие с помощию match = re.search(size, b)')
+    #match = re.search(re.sub(x,r'\\D',size), size)
     #print(match[0] if match else 'Not found')
     #
     #
     #
+    #берем материал
+    material = sheet.cell(row=r, column=c+2).value
+    #
+    #
     #объединяем название товара и шаблон размера в одну строку
-    return re.sub('O-ring','Кольцо круглого сечения',sheet.cell(row=r, column=c).value)+' '+a
+    return item + ' ' + size
 print(pattern())

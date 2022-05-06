3fd4b65e8ff17bd4efc1d19098ccf3257a3673d9
Oleg-ISS <62930142+Oleg-ISS@users.noreply.github.com>
2022-05-06 18:48:19 +0200

    Update creating_string_in_template_file.py

--- creating_string_in_template_file.py
+++ creating_string_in_template_file.py
@@ -1,9 +1,10 @@
 import openpyxl #Подключаем библиотеку Openpyxl
-book = openpyxl.load_workbook("/home/igor/python/template.xlsx") #Открываем тестовый Excel файл
-sheet = book.active
-#worksheet = book['Sheet1'] #Делаем его активным
-r=1
-c=0#колонки начинаются с нуля
-sheet[r][c].value='We are writing to B4' #В указанную ячейку на активном листе пишем все, что в кавычках
-book.save("/home/igor/python/template.xlsx") #Сохраняем измененный файл
-book.close()
+def creating_string_in_template_file(row)
+    book_template = openpyxl.load_workbook("/home/igor/python/template.xlsx") #Открываем тестовый Excel файл
+    sheet_template = book.active
+    #worksheet = book['Sheet1'] #Делаем его активным
+    r=1
+    c=0#колонки начинаются с нуля
+    sheet_template[r][c].value='We are writing to B4' #В указанную ячейку на активном листе пишем все, что в кавычках
+    book_template.save("/home/igor/python/template.xlsx") #Сохраняем измененный файл
+    book_template.close()

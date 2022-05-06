6a2c6ecff80511420f2946cf3ed6c29545ce2bdc
Oleg-ISS <62930142+Oleg-ISS@users.noreply.github.com>
2022-05-06 08:03:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,16 +8,17 @@ book_invoice = openpyxl.load_workbook("invoice.xlsx", read_only=True)
 sheet_invoice = book.active
 #получаем количество строк на рабочем листе
 i = sheet_invoice.max_row
-nazvanie = sheet_invoice.cell(row=r, column=c).value
-razmer = sheet_invoice.cell(row=r, column=c+1).value
-material = sheet_invoice.cell(row=r, column=c+2).value
-#создаем шаблон-строку для поиска по БД на основе файла invoice.xlsx 
-template = creating_template(nazvanie,razmer,material)
-#запускаем функцию поиска строки, которая соответствует шаблону-строке в файле DB.xlsx
-searching(template)
-#открываем на запись файл-шаблон для импорта счета в мой склад
-#запускаем функцию которая создаст шаблон из строки инвойса
-#передадим шаблон в функцию поиска в базе мой склад
-#если в базе есть строка соответствующая шаблону запустим функцию записи в файл-шаблон мой склад
-#если строка не найдена запустим функцию записи состоянии не нашел
+for row in range(1,i+1):
+    nazvanie = sheet_invoice.cell(row=r, column=c).value
+    razmer = sheet_invoice.cell(row=r, column=c+1).value
+    material = sheet_invoice.cell(row=r, column=c+2).value
+    #создаем шаблон-строку для поиска по БД на основе файла invoice.xlsx 
+    template = creating_template(nazvanie,razmer,material)
+    #запускаем функцию поиска строки, которая соответствует шаблону-строке в файле DB.xlsx
+    searching(template)
+    #открываем на запись файл-шаблон для импорта счета в мой склад
+    #запускаем функцию которая создаст шаблон из строки инвойса
+    #передадим шаблон в функцию поиска в базе мой склад
+    #если в базе есть строка соответствующая шаблону запустим функцию записи в файл-шаблон мой склад
+    #если строка не найдена запустим функцию записи состоянии не нашел
 print("Работа программы завершена")

0197bf5cc7d627cf725df29d3df13521dbbd963f
Oleg-ISS <62930142+Oleg-ISS@users.noreply.github.com>
2022-05-06 17:59:17 +0200

    Update creating_string_in_template_file.py

--- creating_string_in_template_file.py
+++ creating_string_in_template_file.py
@@ -1,1 +1,11 @@
-
+# import openpyxl module
+import openpyxl
+# Call a Workbook() function of openpyxl 
+# to create a new blank Workbook object
+book = openpyxl.Workbook()
+sheet = book.active
+r=1
+c=1
+sheet.cell(row=r, column=c) = 1
+book.save("/home/igor/python/template.xlsx")
+book.close()

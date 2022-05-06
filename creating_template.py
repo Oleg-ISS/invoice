8fb1e962937e80d3b6c408abb3a6f07cd5e51536
Oleg-ISS <62930142+Oleg-ISS@users.noreply.github.com>
2022-05-06 21:07:29 +0200

    Update creating_template.py

--- creating_template.py
+++ creating_template.py
@@ -6,7 +6,7 @@ def creating_template():
     r=1
     c=1
     book_invoice = openpyxl.load_workbook("invoice.xlsx", read_only=True)
-    sheet_invoice = book.active
+    sheet_invoice = book_invoice.active
     print(sheet.cell(row=r, column=c).value)
     #По очереди пытаемся заменить название изделия с поставщика на наше, если получиться то меняем если нет то остается как было
     item = re.sub('O-ring','Кольцо круглого сечения',sheet_invoice.cell(row=r, column=c).value)

2486f67dfe0362c2f0b929ee64febda22991c63c
Oleg-ISS <62930142+Oleg-ISS@users.noreply.github.com>
2022-05-06 19:10:36 +0200

    Update creating_string_in_template_file.py

--- creating_string_in_template_file.py
+++ creating_string_in_template_file.py
@@ -1,10 +1,18 @@
 import openpyxl #Подключаем библиотеку Openpyxl
-def creating_string_in_template_file(row)
+def creating_string_in_template_file(r)
+    #берем из DB.xlsx сроку что бы создать запись в файле template.xlsx
+    book_DB = openpyxl.load_workbook("/home/igor/python/DB.xlsx", read_only=True)
+    sheet_DB = book.active
+    kod = sheet_DB.cell(row=r, column=1).value
+    artikul = sheet_DB.cell(row=r, column=2).value
+    naimenovanie = sheet_DB.cell(row=r, column=3).value
     book_template = openpyxl.load_workbook("/home/igor/python/template.xlsx") #Открываем тестовый Excel файл
     sheet_template = book.active
     #worksheet = book['Sheet1'] #Делаем его активным
     r=1
     c=0#колонки начинаются с нуля
-    sheet_template[r][c].value='We are writing to B4' #В указанную ячейку на активном листе пишем все, что в кавычках
+    sheet_template[r][0].value= kod  #
+    sheet_template[r][1].value= artikul  #
+    sheet_template[r][2].value= naimenovanie  #
     book_template.save("/home/igor/python/template.xlsx") #Сохраняем измененный файл
     book_template.close()

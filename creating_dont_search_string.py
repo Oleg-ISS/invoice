08f2adc3e377d0faf8f7a00430338e9d1e38081b
Oleg-ISS <62930142+Oleg-ISS@users.noreply.github.com>
2022-05-07 09:24:20 +0200

    Update creating_dont_search_string.py

--- creating_dont_search_string.py
+++ creating_dont_search_string.py
@@ -1,5 +1,5 @@
 #фукция создает запись в файле invoice.xlsx запись в пустой ячейки справа "не найден"
 def creating_dont_search_strin()
-    global row, sheet_invoice
-    sheet_invoice[row][6] = "не найден"#записываем в ячейку в колонке G
+    global i_main, sheet_invoice
+    sheet_invoice[i_main][6] = "не найден"#записываем в ячейку в колонке G
 

7d0ca349ba16a20bbd4a05480348e0b1b9781a09
Oleg-ISS <62930142+Oleg-ISS@users.noreply.github.com>
2022-05-08 21:45:55 +0200

    Update creating_dont_search_string.py

--- creating_dont_search_string.py
+++ creating_dont_search_string.py
@@ -2,4 +2,5 @@
 def creating_dont_search_string()
     global i_main, sheet_invoice
     sheet_invoice[i_main][6] = "не найден"#записываем в ячейку в колонке G
+    book_invoice.save("/home/igor/python/invoice.xlsx") #Сохраняем измененный файл
 

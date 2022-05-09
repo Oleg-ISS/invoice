f1d4463f2a16235bdd0b71b9d1c80c2087c490fc
Oleg-ISS <62930142+Oleg-ISS@users.noreply.github.com>
2022-05-09 20:12:49 +0200

    Update creating_template_2.py

--- creating_template_2.py
+++ creating_template_2.py
@@ -1,10 +1,10 @@
 def creating_template(nazvanie,razmer,material):
     item = 0
-    item = re.sub('O-ring','Кольцо круглого сечения',nazvanie)
+    item = re.sub('O-ring cord','Шнур круглого сечения',nazvanie)
     if item == nazvanie:
         item = re.sub('Oil Seal','Манжета армированная',nazvanie)
     if item == nazvanie:
-        item = re.sub('cord','Шнур',nazvanie)
+        item = re.sub('O-ring','Кольцо круглого сечения',nazvanie)
     size = re.sub('x',r'\\D',razmer)
     size = re.sub('\*',r'\\D',razmer)
     return item + ' ' + size + ' ' + material

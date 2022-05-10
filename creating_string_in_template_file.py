305c03f8d8fb0e26785ca246653d54f9eb036c91
Oleg-ISS <62930142+Oleg-ISS@users.noreply.github.com>
2022-05-10 11:42:29 +0300

    Update creating_string_in_template_file.py

--- creating_string_in_template_file.py
+++ creating_string_in_template_file.py
@@ -3,16 +3,14 @@ def creating_string_in_template_file(r):
     #берем из DB.xlsx сроку что бы создать запись в файле template.xlsx
     book_DB = openpyxl.load_workbook("DB.xlsx", read_only=True)
     sheet_DB = book_DB.active
-    kod = sheet_DB.cell(row=r, column=1).value
-    artikul = sheet_DB.cell(row=r, column=2).value
-    naimenovanie = sheet_DB.cell(row=r, column=3).value
+    kod = sheet_DB.cell(row=r, column=2).value
+    #artikul = sheet_DB.cell(row=r, column=3).value
+    naimenovanie = sheet_DB.cell(row=r, column=4).value
     book_template = openpyxl.load_workbook("template.xlsx") #Открываем тестовый Excel файл
     sheet_template = book_template.active
     #worksheet = book['Sheet1'] #Делаем его активным
-    r=1
-    c=0#колонки начинаются с нуля
-    sheet_template[r][0].value= kod  #
-    sheet_template[r][1].value= artikul  #
-    sheet_template[r][2].value= naimenovanie  #
-    book_template.save("/home/igor/python/template.xlsx") #Сохраняем измененный файл
+    sheet_template[coordinates_kod].value= kod  #
+    sheet_template[coordinates_artikul].value= artikul  #
+    sheet_template[coordinates_naimenovanie].value= naimenovanie  #
+    book_template.save("template.xlsx") #Сохраняем измененный файл
     book_template.close()

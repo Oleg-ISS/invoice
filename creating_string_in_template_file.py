84cbe7882b7393df98bd3e92d1595800835485dc
Oleg-ISS <62930142+Oleg-ISS@users.noreply.github.com>
2022-05-10 12:56:06 +0300

    Update creating_string_in_template_file.py

--- creating_string_in_template_file.py
+++ creating_string_in_template_file.py
@@ -1,9 +1,10 @@
-import openpyxl #Подключаем библиотеку Openpyxl
+#import openpyxl #Подключаем библиотеку Openpyxl
 def creating_string_in_template_file(r):
     #берем из DB.xlsx сроку что бы создать запись в файле template.xlsx
-    book_DB = openpyxl.load_workbook("DB.xlsx", read_only=True)
-    sheet_DB = book_DB.active
+    #book_DB = openpyxl.load_workbook("DB.xlsx", read_only=True)
+    #sheet_DB = book_DB.active
     kod = sheet_DB.cell(row=r, column=2).value
+    coordinates_kod = 
     #artikul = sheet_DB.cell(row=r, column=3).value
     naimenovanie = sheet_DB.cell(row=r, column=4).value
     book_template = openpyxl.load_workbook("template.xlsx") #Открываем тестовый Excel файл
